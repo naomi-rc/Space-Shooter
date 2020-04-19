78fc793a1e65c049aa6f6d167b220a4b6b3debef
naomi-rc <naomi.cat182128@gmail.com>
2020-04-19 15:57:42 -0400

    add player object, movement and shooting; cooldown system and code cleanup

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 8.0f;
+    private Vector3 direction;
+    private float _upperBound = 8.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = new Vector3(0, 1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+        if(transform.position.y >= _upperBound)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,17 +7,23 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.5f;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+
     private const float LEFT_BOUND = -11.3f;
     private const float RIGHT_BOUND = 11.3f;
     private const float UPPER_BOUND = 0;
     private const float LOWER_BOUND = -3.8f;
+    private Vector3 offset;
+    private float _canFire = -1f;
 
     // Start is called before the first frame update
     void Start()
-    {
-        //take current position = new position (0, 0, 0)
+    {        
         transform.position = new Vector3(0, 0, 0);
-        
+        offset = new Vector3(0, 0.8f, 0);
     }
 
     // Update is called once per frame (60 frames per second)
@@ -25,34 +31,22 @@ public class Player : MonoBehaviour
     {
         CalculateMovement();
 
+        if (FirePressed() && Time.time > _canFire)
+        {
+            FireLaser();
+        }
     }
 
     void CalculateMovement() {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        //transform.Translate(Vector3.right); //1 unity unit = 1 meter so this is moving 60m/s
-        //transform.Translate(Vector3.right * horizontalInput* _speed * Time.deltaTime);
-        //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+       
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
         transform.Translate(direction * _speed * Time.deltaTime);
-
-        //if player position on y > 0, y position = 0
-        //else if position on y < -3.8f, y position = -3.8f
-       /* if (transform.position.y >= UPPER_BOUND)
-        {
-            transform.position = new Vector3(transform.position.x, UPPER_BOUND, 0);
-        }
-        else if (transform.position.y <= LOWER_BOUND)
-        {
-            transform.position = new Vector3(transform.position.x, LOWER_BOUND, 0);
-        }*/
-
-        //clamp position instead of else-if 
+       
         transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, LOWER_BOUND, UPPER_BOUND), 0);
 
-        //if player position on x <= LEFT_BOUND, x position = RIGHT_BOUND
-        //else if player position on x >= RIGHT_BOUND, x position = LEFT_BOUND
         if (transform.position.x <= LEFT_BOUND)
         {
             transform.position = new Vector3(RIGHT_BOUND, transform.position.y, 0);
@@ -62,4 +56,15 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(LEFT_BOUND, transform.position.y, 0);
         }
     } 
+
+    private void FireLaser()
+    {
+        _canFire = Time.time + _fireRate;
+        Instantiate(_laserPrefab, transform.position + offset, Quaternion.identity);
+    }
+
+    private bool FirePressed()
+    {       
+        return Input.GetKey(KeyCode.Space) || Input.GetAxis("Fire1") != 0 || Input.GetAxis("Fire2") != 0 || Input.GetAxis("Fire3") != 0;
+    }
 }
