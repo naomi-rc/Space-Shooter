71a7974b306971a2a820c364bc9134649fd64b86
naomi-rc <naomi.cat182128@gmail.com>
2020-04-29 16:50:09 -0400

    commit to save

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    Random random;
+    // Start is called before the first frame update
+    void Start()
+    {
+        random = new Random();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float horizontalPosition = 
+        //transform.position = new Vector3(horizontalPosition, verticalPosition, 0);
+    }
+}
